2927bc3226a196ce45c6cf71de0d65569134e58a
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-11-25 06:21:28 -0500

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+//Add middleware to protect routes by validating the JWT token
 const authenticateJWT = (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
     if (token) {
