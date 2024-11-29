0cbb13e9f6eaf7e84fcf4b8e73f0cf0dc061818b
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-11-29 03:21:39 -0500

    Update AccessController.ts

--- src/controller/AccessController.ts
+++ src/controller/AccessController.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from "express";
-
 import { prisma } from "../database/prisma";
+import { Prisma } from "@prisma/client";
 
 export const createAccess = async (req: Request, res: Response) => {
-  const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-  const access = await prisma.access.create({
-    data: { name },
-  });
+    const access = await prisma.access.create({
+      data: { name },
+    });
 
-  return res.json(access);
+    return res.status(201).json(access);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        return res.status(400).json({ 
+          message: "Ya existe un acceso con ese nombre" 
+        });
+      }
+    }
+    return res.status(500).json({ 
+      message: "Error interno del servidor" 
+    });
+  }
 };
 
 
