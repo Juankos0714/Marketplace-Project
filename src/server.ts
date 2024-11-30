85a0fd6f308a475ec482a9321e211151d39089df
Miguel vargas Navarro <mvargasnavarro19@gmail.com>
2024-11-30 10:10:59 -0500

    const port

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,6 @@ import { router } from "./router";
 const app = express();
 app.use(express.json())
 app.use(router);
+const port: string = process.env.PORT || '3333';
 
-app.listen(3333, () => console.log("Server started"));
+app.listen(`${port}`, () => console.log(`Server running on port ${port} - http://localhost:${port}`));
