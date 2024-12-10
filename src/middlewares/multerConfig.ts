8e417eb79a1336f9bc138051e2014a05f0deba98
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-12-09 22:39:16 -0500

    Update multerConfig.ts
    Modifique el multer

--- src/middlewares/multerConfig.ts
+++ src/middlewares/multerConfig.ts
@@ -4,7 +4,7 @@ import path from 'path';
 // Configure multer for image upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'src/public/images');
+    cb(null, '../../public/images/products');
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
